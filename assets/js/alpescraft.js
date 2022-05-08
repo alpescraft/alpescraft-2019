6d54ec0f5e28425afff183b24c9aee07a372df93
Victor Gallet <victor.gallet@protonmail.ch>
2022-05-08 14:30:06 +0200

    Work on accessibility, greenIT & performance
     * add aria-label
     * minified js & css
     * use webp format for pictures
     * fix navbar issues

---
+++ assets/js/alpescraft.js
@@ -0,0 +1,36 @@
+    function getURLParameter(sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }
+
+    var talk = getURLParameter('talk');
+
+    $('#' + talk).show();
+
+    window.addEventListener('scroll', scrollFunction);
+
+    function scrollFunction() {
+        setTimeout(function() {
+            const distanceY = window.pageYOffset || $(document).scrollTop();
+
+            if (distanceY > 55) {
+                document.getElementById("top-button").style.display = "block";
+                $('nav').addClass('shrink');
+            } else {
+                document.getElementById("top-button").style.display = "none";
+                $('nav').removeClass('shrink');
+            }
+        }, 1000);
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
