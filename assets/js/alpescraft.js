d8a33d54f1d900146b546aa5f6cb93deff0cc0b8
Victor Gallet <victor.gallet@protonmail.ch>
2022-05-13 12:17:48 +0200

    fix session link

--- assets/js/alpescraft.js
+++ assets/js/alpescraft.js
@@ -1,18 +1,3 @@
-    function getURLParameter(sParam) {
-        var sPageURL = window.location.search.substring(1);
-        var sURLVariables = sPageURL.split('&');
-        for (var i = 0; i < sURLVariables.length; i++) {
-            var sParameterName = sURLVariables[i].split('=');
-            if (sParameterName[0] == sParam) {
-                return sParameterName[1];
-            }
-        }
-    }
-
-    var talk = getURLParameter('talk');
-
-    $('#' + talk).show();
-
     window.addEventListener('scroll', scrollFunction);
 
     function scrollFunction() {
