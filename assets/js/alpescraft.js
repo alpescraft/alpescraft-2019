7b2389134931cf5d51402172e9d4eed95a0f62b0
Victor Gallet <victor.gallet@protonmail.ch>
2022-05-28 15:49:46 +0200

    Add Speaker Carousel
     * use slick to create a responsive carousel

--- assets/js/alpescraft.js
+++ assets/js/alpescraft.js
@@ -19,3 +19,67 @@
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+
+
+// bootstrap 4 breakpoints
+const breakpoint = {
+  // small screen / phone
+  sm: 576,
+  // medium screen / tablet
+  md: 768,
+  // large screen / desktop
+  // extra large screen / wide desktop
+  xl: 1200
+};
+
+// bootstrap 4 responsive multi column slick carousel
+$('#slick').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  draggable: false,
+  pauseOnHover: true,
+  infinite: true,
+  dots: false,
+  arrows: false,
+  speed: 1000,
+  variableWidth: false,
+
+  mobileFirst: true,
+
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+  responsive: [{
+      breakpoint: breakpoint.sm,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        variableWidth: false
+      }
+    },
+    {
+      breakpoint: breakpoint.md,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: false
+      }
+    },
+    {
+      breakpoint: breakpoint.lg,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        variableWidth: false
+      }
+    },
+    {
+      breakpoint: breakpoint.xl,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        variableWidth: false
+      }
+    }
+  ]
+});
